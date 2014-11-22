44c0d0e4232f89340a9b07f3bdd8ee44298514b0
Wojciech Zatorski <wojciech@zatorski.net>
2014-11-22 21:33:43 +0100

    new release

--- cpp/examples/MinOZW/Main.cpp
+++ cpp/examples/MinOZW/Main.cpp
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 //
-//	Main.cpp v0.20141120
+//	Main.cpp v0.20141116
 //
 //	Based on minimal application to test OpenZWave.
 //
